eecfab2c3e9e40542d339d160f5905aa01a70c7e
Nick Wanninger <nickwanninger@gmail.com>
2018-01-08 20:57:11 -0600

    fixed vim install

--- setup.sh
+++ setup.sh
@@ -1,19 +1,25 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 ln -sF ~/dotfiles/fish/ ~/.config
 
-
-ln -sF ~/dotfiles/vim/vimrc ~/.nvimrc
-ln -sF ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sF ~/dotfiles/vim/ ~/.vim
-ln -sF ~/dotfiles/vim/ ~/.config/nvim
 
-ln -sF ~/dotfiles/git/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/git/.gitignore ~/.gitignore
 
-ln -sF ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
-defaults write com.apple.dock orientation -string left
-defaults write com.apple.dock autohide-time-modifier -float 0.12
-defaults write com.apple.dock autohide-delay -float 0
-killall Dock
+# defaults write com.apple.dock orientation -string left
+# defaults write com.apple.dock autohide-time-modifier -float 0.12
+# defaults write com.apple.dock autohide-delay -float 0
+# killall Dock
+
+BUNDLE_DIR=~/.vim/bundle
+
+# Install/update Vundle
+mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/gmarik/vundle.git "$BUNDLE_DIR/vundle" || (cd "$BUNDLE_DIR/vundle" && git pull origin master))
+
+# Install bundles
+vim +PluginInstall +qall
+
 echo "Setup Complete!"
