0caff55c910e835221a1a15fdd323b618d5b2499
Nick Wanninger <nickwanninger@gmail.com>
2018-01-08 21:06:03 -0600

    vundle edits

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-
+rm -rf ~/.config/fish
 ln -sF ~/dotfiles/fish/ ~/.config
 
+rm -rf ~/.vim
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sF ~/dotfiles/vim/ ~/.vim
 
@@ -14,12 +15,4 @@ ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 # defaults write com.apple.dock autohide-delay -float 0
 # killall Dock
 
-BUNDLE_DIR=~/.vim/bundle
 
-# Install/update Vundle
-mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/gmarik/vundle.git "$BUNDLE_DIR/vundle" || (cd "$BUNDLE_DIR/vundle" && git pull origin master))
-
-# Install bundles
-vim +PluginInstall +qall
-
-echo "Setup Complete!"
---
+++ vundle.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+BUNDLE_DIR=~/.vim/bundle
+rm -rf "$BUNDLE_DIR/*"
+# Install/update Vundle
+mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/gmarik/vundle.git "$BUNDLE_DIR/vundle" || (cd "$BUNDLE_DIR/vundle" && git pull origin master))
+
+# Install bundles
+vim +PluginInstall +qall
+
+echo "Setup Complete!"
