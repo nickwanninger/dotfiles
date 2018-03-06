2d0572cab0482557a48162f4152e90993a8620fe
Nick Wanninger <nickwanninger@gmail.com>
2018-03-06 14:39:44 -0600

    prompt update

--- fish/config.fish
+++ fish/config.fish
@@ -2,15 +2,23 @@
 set fish_greeting ""
 set HOMEBREW_NO_AUTO_UPDATE 1
 export TERM="xterm-256color"
-
+# Show a pretty calendar :)
+# cal
 switch (uname)
 case Darwin
-  export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+  # export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 end
+
+
 export PATH="$HOME/.cargo/bin:$PATH"
+
+export PATH="$HOME/.bin:$PATH"
 export EDITOR=(which vim)
 set -x GOPATH $HOME/dev/go/
 
 alias t "tmux"
 alias v "vim"
 alias :q "exit"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/nick/.gcloud/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/nick/.gcloud/google-cloud-sdk/path.fish.inc'; else; . '/Users/nick/.gcloud/google-cloud-sdk/path.fish.inc'; end; end
