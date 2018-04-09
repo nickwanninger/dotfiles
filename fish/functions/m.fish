a9b64b1840aef78f72f34cf06e49833962844fc6
Nick Wanninger <nickwanninger@gmail.com>
2018-04-08 23:59:13 -0500

    update

---
+++ fish/functions/m.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function m --description 'alias m /Applications/Mathematica.app/Contents/MacOS/MathKernel'
+	/Applications/Mathematica.app/Contents/MacOS/MathKernel  $argv;
+end
