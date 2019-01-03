f9bcc8e71b49f5d72d82a5433d0a51a10df98a67
Nick Wanninger <nickwanninger@gmail.com>
2019-01-03 01:00:00 -0600

    update Thu Jan  3 01:00:00 CST 2019

--- vim/.ycm_extra_conf.py
+++ vim/.ycm_extra_conf.py
@@ -3,10 +3,10 @@ import os
 def FlagsForFile( filename, **kwargs ):
     name, ext = os.path.splitext(filename)
     lang = "c++"
-    lib = "-std=c++17"
+    std = "-std=c++17"
     if ext == ".c":
         lang = "c"
-        lib = "-std=c99"
+        std = "-std=c99"
     return {
-           'flags': [ '-x', lang, lib, '-Wall', '-I./include', '-Wno-unused-variable', '-fms-extensions', '-Wno-gnu-anonymous-struct' ]
+           'flags': [ '-x', lang, std, '-Wall', '-I./include', '-I./Include', '-Wunused-variable', '-fms-extensions', '-Wno-gnu-anonymous-struct' ]
             }
