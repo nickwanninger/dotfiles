87ae2f86d6a711c1ff25f76a1114e8c2ec9c6df5
Nick Wanninger <nickwanninger@gmail.com>
2019-02-01 00:00:01 -0600

    update Fri Feb  1 00:00:00 CST 2019

--- fish/config.fish
+++ fish/config.fish
@@ -47,6 +47,6 @@ set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 
 switch (uname)
 case Darwin
-	export CC=/usr/local/opt/llvm/bin/clang
-	export CXX=/usr/local/opt/llvm/bin/clang++
+	# export CC=/usr/local/opt/llvm/bin/clang
+	# export CXX=/usr/local/opt/llvm/bin/clang++
 end
