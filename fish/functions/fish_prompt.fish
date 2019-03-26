d5318bd6c09670b35d7aa16f287015ad573f2c81
Nick Wanninger <nickwanninger@gmail.com>
2019-03-26 16:00:00 -0500

    update Tue Mar 26 16:00:00 CDT 2019

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,6 +4,11 @@ function fish_prompt
 
 
 
+	set_color cyan
+	printf (prompt_pwd)
+
+	return
+
 	set_color "#44475A"
 	printf (prompt_hostname)
 	printf " "
@@ -17,8 +22,9 @@ function fish_prompt
 		set_color green
 	end
 
+
 	set_color "#44475A"
-	printf ":: "
+	printf " ⌘ "
 
 end
 
