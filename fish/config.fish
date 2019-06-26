1ad196d30de36bef95c1551a68929c9550bcfaad
Nick Wanninger <nickwanninger@gmail.com>
2019-06-25 19:00:00 -0500

    update Tue Jun 25 19:00:00 CDT 2019

--- fish/config.fish
+++ fish/config.fish
@@ -15,8 +15,6 @@ case Darwin
 	export PATH="$HOME/.bin:$PATH"
 	export PATH="$HOME/.cargo/bin:/opt/local/bin:$PATH"
 	export PATH="$GOPATH/bin:$PATH"
-	export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
-
 end
 
 
