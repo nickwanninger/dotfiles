157096f97a559a2b528f1b4c771eb6ce7b2469b5
Nick Wanninger <nickwanninger@gmail.com>
2019-07-30 23:40:42 -0500

    update

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -22,8 +22,35 @@ function __fish_prompt
 end
 
 function fish_prompt
-	set_color green
-	printf "%s\n" (prompt_pwd)
+
+	set -l exit_status $status
+
+	if [ $exit_status -ne 0 ]
+		printf "["
+		printf "%d" $exit_status
+		printf "] "
+	end
+
+	if [ (prompt_pwd) != "~" ]
+		set_color green
+		printf "%s" (prompt_pwd)
+
+		# show git file changes
+		set -l file_changes (git diff --name-only 2> /dev/null | wc -l)
+		if [ $file_changes -ne 0 ]
+			set_color normal
+			printf " <%d>" $file_changes
+		end
+
+		set -l branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+		if [ "$branch" != "" ]
+			printf " (%s)" "$branch"
+		end
+
+
+		printf "\n"
+	end
+
 	set_color yellow
 	printf "%s" (prompt_hostname)
 	set_color normal
