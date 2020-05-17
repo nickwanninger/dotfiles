09cb2fe91b898fda881dc4c4cdc41d301c1ad23a
Nick Wanninger <nickwanninger@gmail.com>
2020-05-17 16:40:59 -0400

    prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,22 +1,41 @@
 function fish_prompt
     set -l status_copy $status
 
-		switch (uname)
-			case Darwin
-				printf " "
-		end
+		# switch (uname)
+		#   case Darwin
+		#      printf "[mac] "
+		# end
 
+
+		echo -n "["
+		set_color brblack
+
+		set_color red
+		echo -n (whoami)
+		set_color brblack
+		echo -n "@"
+
+		set_color red
+		echo -n (hostname)
 
 		set_color brblack
-		printf "%s " (prompt_pwd)
+		echo -n "::"
+
+		set_color cyan
+		echo -n (prompt_pwd)
+
+		set_color reset
+		echo -n "] "
 
     if test "$status_copy" -ne 0
 				set_color red
 				printf "[%d] " $status_copy
 		else
-			#set_color green
+			set_color green
 			#printf "| "
     end
+
+		printf "\$ "
 
 		set_color normal
 
