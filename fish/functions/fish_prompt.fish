721ddc177eb6e130c8773e3d98609d1e38fa25a7
Nick Wanninger <nickwanninger@gmail.com>
2020-07-03 16:44:02 -0500

    solarized

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -14,29 +14,29 @@ function fish_prompt
 		# set_color white
 		# echo -n "@"
 
-		set_color 5ad7ff
+		# set_color 5ad7ff
 		echo -n (hostname)
 
-		set_color 8a8a8a
+		# set_color 8a8a8a
 		echo -n " "
 
-		set_color 5fd7af
+		# set_color 5fd7af
 		# echo -n (basename (prompt_pwd))
 		echo -n (prompt_pwd)
 
-		set_color reset
+		# set_color reset
 		echo -n "]"
 
     if test "$status_copy" -ne 0
 				set_color red
 				printf "[%d] " $status_copy
 		else
-			set_color 8a8a8a
+			# set_color 8a8a8a
 			#printf "| "
     end
 
 		printf "\$ "
 
-		set_color normal
+		# set_color normal
 
 end
