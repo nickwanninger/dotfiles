dcabc8ea221b395404c1dc27d81a274cad006953
Nick Wanninger <nickwanninger@gmail.com>
2021-02-21 20:43:26 -0600

    fish: fix terrible performance?

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,18 @@
 # Remove the greeting from the fish prompt on first open
+#
 set fish_greeting ""
-set HOMEBREW_NO_AUTO_UPDATE 1
 export TERM="xterm-256color"
 set -x GOPATH $HOME/dev/go
 
+
 mkdir -p ~/.local/bin
-export PATH="$HOME/.local/bin:$PATH"
+# export PATH="$HOME/.local/bin:$PATH"
+
 
 switch (uname)
 # Specific things to my mac
 case Darwin
-	export PATH="$PATH:/usr/local/bin:/usr/local/sbin"
-	# export PATH="$PATH:/usr/local/opt/python@2/libexec/bin"
-
-	# latex crap
-	# export PATH="/usr/local/texlive/2019/bin/x86_64-darwin:$PATH"
-	export PATH="$PATH:$HOME/.bin"
-	export PATH="$PATH:$HOME/.cargo/bin:/opt/local/bin"
-	# export PATH="$PATH:$GOPATH/bin"
+	# export PATH="$PATH:/usr/local/bin:/usr/local/sbin"
 end
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
@@ -25,8 +20,11 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
 
 # Setup ~/.bin as a PATH location.
 mkdir -p ~/.bin
-export PATH="$HOME/.bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
+
+# export PATH="$HOME/.bin:$PATH"
+# export PATH="$HOME/.cargo/bin:$PATH"
+
+set -g fish_user_paths $HOME/.bin $HOME/.local/bin $HOME/.cargo/bin $fish_user_paths
 
 export EDITOR=(which nvim)
 
@@ -44,22 +42,8 @@ alias glog "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset
 alias lg "lazygit"
 alias lx "exa -lgaFBS"
 
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/nick/.gcloud/google-cloud-sdk/path.fish.inc' ];
-	if type source > /dev/null;
-		source '/Users/nick/.gcloud/google-cloud-sdk/path.fish.inc';
-	else;
-		. '/Users/nick/.gcloud/google-cloud-sdk/path.fish.inc';
-	end;
-end
-
 # disable homebrew auto update. Not sure if this works
 set -x HOMEBREW_NO_AUTO_UPDATE 1
-set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/binutils/bin" $fish_user_paths
+set HOMEBREW_NO_AUTO_UPDATE 1
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-
-# set "__fish_git_prompt_show_informative_status" yes
-set "__fish_git_prompt_showdirtystate" yes
