c0a6fc3bd75cec74b34aa836409c2ae9710b8090
Nick Wanninger <ncw@u.northwestern.edu>
2021-12-07 10:55:29 -0600

    update

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt -d Hydro
-    string unescape "$_hydro_color_pwd$hostname\x1b[35m : $_hydro_color_pwd$_hydro_pwd\x1b[0m $_hydro_color_git$$_hydro_git\x1b[0m$_hydro_color_duration$_hydro_cmd_duration\n\x1b[0m$_hydro_prompt\x1b[0m "
+    string unescape "$_hydro_color_pwd$hostname $_hydro_color_pwd$_hydro_pwd\x1b[0m $_hydro_prompt\x1b[0m "
 end
