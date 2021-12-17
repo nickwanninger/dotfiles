88b8104e3919b09f03e980d21fd57023caef5b07
Nick Wanninger <ncw@u.northwestern.edu>
2021-12-16 19:50:41 -0600

    update

---
+++ vim/lua/init.lua
@@ -0,0 +1,57 @@
+
+local nvim_lsp = require 'lspconfig'
+local cmp = require 'cmp'
+local saga = require 'lspsaga'
+
+
+nvim_lsp.clangd.setup({})
+
+saga.init_lsp_saga()
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+lsp_sig = require('lsp_signature')
+lsp_sig.setup {
+	bind = true,
+	hint_enable = false,
+}
+
+
+require('nvim-treesitter.configs').setup {}
+
