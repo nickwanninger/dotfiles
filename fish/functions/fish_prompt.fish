0a07eadc908b4e199e4c84482c5ddaa7271717b1
Nick Wanninger <nickwanninger@gmail.com>
2023-02-26 10:50:53 -0500

    update

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,21 +1,38 @@
 function fish_prompt -d Hydro
 
-	set_color green
-	echo -n (whoami)
+	# set_color green
+	# echo -n (whoami)
+	# set_color reset
+	# echo -n "@"
+	# set_color blue
+	# echo -n (hostname)
+	# set_color brblack
+	# echo -n ":"
+	# set_color green
+	# echo -n (prompt_pwd)
+	
+	# set_color green
+	# printf '┌ '
+
+
 	set_color reset
-	echo -n "@"
+	if [ $SSH_TTY ];
+		echo -n (hostname)
+		echo -n ' '
+	end
+
 	set_color blue
-	echo -n (hostname)
-	set_color brblack
-	echo -n ":"
-	set_color green
+
 	echo -n (prompt_pwd)
 
+
+	echo -en '\\n'
+
+	# set_color green
+	# printf '└ '
+	set_color reset
 	set_color brblack
 	echo -n '$ '
 	set_color reset
-
-	# set_color reset
-
 
 end
