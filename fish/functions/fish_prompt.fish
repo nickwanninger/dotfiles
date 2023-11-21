914aa98b22d391bccdb623a1e619093adc6488d7
Nick Wanninger <nickwanninger@gmail.com>
2023-11-21 15:12:53 -0600

    update prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,41 +1,44 @@
-function fish_prompt -d Hydro
+function fish_prompt
 
-	# set_color green
-	# echo -n (whoami)
-	# set_color reset
-	# echo -n "@"
-	# set_color blue
-	# echo -n (hostname)
-	# set_color brblack
-	# echo -n ":"
-	# set_color green
-	# echo -n (prompt_pwd)
+  set last_status $status
 	
 	# set_color green
 	# printf '┌ '
 
-	# set -g __fish_git_prompt_show_informative_status 1
+	set -g __fish_git_prompt_show_informative_status 1
 
-	set_color reset
-	if [ $SSH_TTY ];
-		echo -n (hostname)
-		echo -n ' '
-	end
+  set status_color "red"
+
+  if [ $last_status -eq "0" ]
+    set status_color "green"
+  end
+
+  set_color $status_color
+  echo -n '╭╴'
+
+  if [ ! $last_status -eq "0" ]
+    echo -n " $last_status "
+  end
+
+  set_color reset
+  set_color -i
+  echo -n (hostname)
+  echo -n ' '
+  set_color reset
 
 	set_color blue
 
 	echo -n (prompt_pwd)
 
-	# set_color brblack
-	# echo -n (fish_git_prompt)
+	set_color brblack
+	echo -n (fish_git_prompt)
 
 
 	echo -en '\\n'
 
-	# set_color green
-	# printf '└ '
-	set_color brblack
-	echo -n '$ '
+  set_color $status_color
+  echo -n '╰╼ '
+
 	set_color reset
 
 end
