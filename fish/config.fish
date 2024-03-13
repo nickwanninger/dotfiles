6d665b65bd96d0a3ccabe351217157ef0a70aee4
Nick Wanninger <nickwanninger@gmail.com>
2024-03-12 19:32:43 -0500

    remove git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -49,17 +49,7 @@ alias glog "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset
 alias lg "nvim -c :Neogit"
 alias lx "exa -lgFBS"
 
-alias gp "git push"
-alias gP "git pull"
-alias gc "git commit"
-alias gcm "git commit -m"
-alias gs "git status"
-alias gsw "git switch"
-
 # cd to the git root
-alias gd "cd (git rev-parse --show-toplevel)"
-alias ea "bass source ~/dev/alaska/enable"
-
 alias src "bass source"
 
 alias magit "nvim -c :Neogit"
