7904b559023d94438e7f3325d285cc0cb6473bf6
Nick Wanninger <nickwanninger@gmail.com>
2024-03-29 16:08:42 -0500

    some sin

--- emacs/init.el
+++ emacs/init.el
@@ -3,15 +3,27 @@
              '("melpa" . "https://melpa.org/packages/") t)
 
 
+(setq custom-file
+    (expand-file-name "custom.el" user-emacs-directory))
 
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(package-selected-packages '(magit)))
-(custom-set-faces)
+
+(defun ensure-package (name)
+  "Ensure a package is installed"
+  (unless (package-installed-p name)
+    (package-install name)))
+
+
+(ensure-package 'evil)
+(ensure-package 'magit)
+(ensure-package 'modus-themes)
+
+
+(require 'evil)
+(evil-mode 1)
+
+
+(load-theme 'modus-operandi 't)
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
@@ -21,4 +33,4 @@
 (setq confirm-kill-emacs nil)
 (setq echo-keystrokes 0.01)
 
-(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+;; (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
