b5b66c879762e2b0130c0a296dbaac0bfe27f087
Nick Wanninger <nickwanninger@gmail.com>
2024-03-30 17:08:37 -0500

    Disable lsp-mode until I can figure it out
    I assume I'll have to bug karl. That'll be exciting

--- emacs/init.el
+++ emacs/init.el
@@ -40,46 +40,34 @@
 
 ;; LSP mode
 
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :hook (prog-mode . lsp-deferred)
+;;   :custom
+;;   (lsp-clients-clangd-executable "ccls") ;; or use ccls package to get call
+;;                                          ;; hierarchy lsp extension
+;;   (lsp-auto-guess-root t)                ;; auto guess root
+;;   (lsp-prefer-capf t)                    ;; using `company-capf' by default
+;;   (lsp-keymap-prefix "C-c l"))
 
-;; (ensure-package 'company)
-;; (ensure-package 'lsp-mode)
-;; (ensure-package 'lsp-ui)
-;;
-;; (require 'lsp-mode)
-;; (require 'company)
-;;
-;; (add-hook 'c++-mode-hook 'lsp)
-;; (add-hook 'c++-mode-hook 'company)
-;; (add-hook 'c++-mode-hook (lambda () (setq company-backends '(company-lsp))))
-
-(use-package lsp-mode
-  :ensure t
-  :hook (prog-mode . lsp-deferred)
-  :custom
-  (lsp-clients-clangd-executable "ccls") ;; or use ccls package to get call
-                                         ;; hierarchy lsp extension
-  (lsp-auto-guess-root t)                ;; auto guess root
-  (lsp-prefer-capf t)                    ;; using `company-capf' by default
-  (lsp-keymap-prefix "C-c l"))
-
-(use-package company
-  :ensure t
-  :hook (prog-mode . company-mode)
-  :bind (:map company-mode-map
-         ([remap completion-at-point] . company-complete))
-  :custom
-  (company-idle-delay 0)
-  (company-echo-delay 0)
-  (company-show-numbers t)
-  (company-require-match nil)
-  (company-tooltip-align-annotations t)
-  (company-backends '(company-capf)))
+;; (use-package company
+;;   :ensure t
+;;   :hook (prog-mode . company-mode)
+;;   :bind (:map company-mode-map
+;;          ([remap completion-at-point] . company-complete))
+;;   :custom
+;;   (company-idle-delay 0)
+;;   (company-echo-delay 0)
+;;   (company-show-numbers t)
+;;   (company-require-match nil)
+;;   (company-tooltip-align-annotations t)
+;;   (company-backends '(company-capf)))
 
 
 (add-hook 'c++-mode-hook 'lsp)
 
 ;; Enable mouse mode cause I like rats
-(xterm-mouse-mode nil)
+(xterm-mouse-mode 1)
 
 (setq confirm-kill-emacs #'y-or-n-p)
 (setq echo-keystrokes 0.01)
@@ -87,3 +75,6 @@
 
 
 (setq make-backup-files nil) ; stop creating ~ files
+
+
+(menu-bar-mode -1) 
