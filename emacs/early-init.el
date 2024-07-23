500f162e4350412b264b7a404081c3f66682731d
Nick Wanninger <nickwanninger@gmail.com>
2024-07-23 15:36:33 -0500

    add straight.el

---
+++ emacs/early-init.el
@@ -0,0 +1,19 @@
+;; Disable package.el from start-up, because using elpaca
+(setq package-enable-at-startup nil)
+
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
