e205d0cd5c68661ce16b709011e32ba10e35cfd8
Nick Wanninger <nickwanninger@gmail.com>
2024-08-06 16:11:53 -0500

    update

--- emacs/init.el
+++ emacs/init.el
@@ -170,6 +170,30 @@
 
 
 
+(use-package lambda-line
+  :straight (:type git :host github :repo "lambda-emacs/lambda-line")
+  :custom
+  (lambda-line-icon-time t) ;; requires ClockFace font (see below)
+  (lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
+  (lambda-line-position 'bottom) ;; Set position of status-line
+  (lambda-line-abbrev nil) ;; abbreviate major modes
+  (lambda-line-hspace "  ")  ;; add some cushion
+  (lambda-line-prefix t) ;; use a prefix symbol
+  (lambda-line-prefix-padding nil) ;; no extra space for prefix
+  (lambda-line-status-invert nil)  ;; no invert colors
+  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+  (lambda-line-gui-mod-symbol " ⬤")
+  (lambda-line-gui-rw-symbol  " ◯")
+  (lambda-line-space-top +.50)  ;; padding on top and bottom of line
+  (lambda-line-space-bottom -.50)
+  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+  :config
+  ;; activate lambda-line
+  (lambda-line-mode)
+  ;; set divider line in footer
+  (when (eq lambda-line-position 'top)
+    (setq-default mode-line-format (list "%_"))
+    (setq mode-line-format (list "%_"))))
 
 
 
@@ -530,7 +554,8 @@
 (defun light-theme ()
   "Select the light theme"
   (interactive)
-  (load-theme 'modus-operandi t))
+  (load-theme 'doom-one-light t))
+  ;; (load-theme 'modus-operandi t))
 
 ;; Set the dark theme right away
 (dark-theme)
@@ -849,14 +874,14 @@ You can use \\[keyboard-quit] to hide the doc."
                             'italic))))
 
 
-(add-to-list 'display-buffer-alist
-             '((lambda (buffer _) (with-current-buffer buffer
-                                    (seq-some (lambda (mode)
-                                                (derived-mode-p mode))
-                                              '(help-mode))))
-               (display-buffer-reuse-window display-buffer-below-selected)
-               (reusable-frames . visible)
-               (window-height . 0.25)))
+;; (add-to-list 'display-buffer-alist
+;;              '((lambda (buffer _) (with-current-buffer buffer
+;;                                     (seq-some (lambda (mode)
+;;                                                 (derived-mode-p mode))
+;;                                               '(help-mode))))
+;;                (display-buffer-reuse-window display-buffer-below-selected)
+;;                (reusable-frames . visible)
+;;                (window-height . 0.25)))
 
 
 
