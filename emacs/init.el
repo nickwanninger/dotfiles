6ff6cdef4a3426c21c2ff3b1d762d4d819f275a9
Nick Wanninger <nickwanninger@gmail.com>
2024-09-19 11:57:33 -0500

    fix performance by nuking the modeline

--- emacs/init.el
+++ emacs/init.el
@@ -57,6 +57,8 @@
   :config
   (setq resize-mini-windows 'grow-only)
   (setq max-mini-window-height 0.2)
+  (setq visible-bell nil)
+  (setq ring-bell-function #'ignore)
   ;; Setup the display-buffer-alist
   (setq display-buffer-alist nil))
   ;; (add-to-list 'display-buffer-alist
@@ -187,31 +189,30 @@
 
 
 
-(use-package lambda-line
-  :straight (:type git :host github :repo "lambda-emacs/lambda-line")
-  :custom
-  (lambda-line-icon-time t) ;; requires ClockFace font (see below)
-  (lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
-  (lambda-line-position 'bottom) ;; Set position of status-line
-  (lambda-line-abbrev nil) ;; abbreviate major modes
-  (lambda-line-hspace "  ")  ;; add some cushion
-  (lambda-line-prefix t) ;; use a prefix symbol
-  (lambda-line-prefix-padding nil) ;; no extra space for prefix
-  (lambda-line-status-invert nil)  ;; no invert colors
-  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
-  (lambda-line-gui-mod-symbol " ⬤")
-  (lambda-line-gui-rw-symbol  " ◯")
-  (lambda-line-space-top +.50)  ;; padding on top and bottom of line
-  (lambda-line-space-bottom -.50)
-  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
-  (lambda-line-vc-symbol " ")
-  :config
-  ;; activate lambda-line
-  (lambda-line-mode)
-  ;; set divider line in footer
-  (when (eq lambda-line-position 'top)
-    (setq-default mode-line-format (list "%_"))
-    (setq mode-line-format (list "%_"))))
+;; (use-package lambda-line
+;;   :straight (:type git :host github :repo "lambda-emacs/lambda-line")
+;;   :custom
+;;   (lambda-line-icon-time nil) ;; requires ClockFace font (see below)
+;;   (lambda-line-position 'bottom) ;; Set position of status-line
+;;   (lambda-line-abbrev nil) ;; abbreviate major modes
+;;   (lambda-line-hspace "  ")  ;; add some cushion
+;;   (lambda-line-prefix t) ;; use a prefix symbol
+;;   (lambda-line-prefix-padding nil) ;; no extra space for prefix
+;;   (lambda-line-status-invert nil)  ;; no invert colors
+;;   (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+;;   (lambda-line-gui-mod-symbol " ⬤")
+;;   (lambda-line-gui-rw-symbol  " ◯")
+;;   (lambda-line-space-top +.50)  ;; padding on top and bottom of line
+;;   (lambda-line-space-bottom -.50)
+;;   (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+;;   (lambda-line-vc-symbol " ")
+;;   :config
+;;   ;; activate lambda-line
+;;   (lambda-line-mode)
+;;   ;; set divider line in footer
+;;   (when (eq lambda-line-position 'top)
+;;     (setq-default mode-line-format (list "%_"))
+;;     (setq mode-line-format (list "%_"))))
 
 
 
